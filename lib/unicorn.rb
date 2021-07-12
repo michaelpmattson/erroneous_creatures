157e717b43d65c1f0221af16b6c72a1d0052f624
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-12 11:41:09 -0600

    Fix errors

--- lib/unicorn.rb
+++ lib/unicorn.rb
@@ -1,20 +1,21 @@
-class Uncorn
+class Unicorn
+  attr_reader   :name
+  attr_accessor :color,
+                :eating
 
-  attr_reader :namee
-  attr_accessor :color, :eating
-
-  def initialze(name, color="white")
+  def initialize(name, color="white")
     @name = name
     @color = color
     @eating = false
   end
 
   def white?
-    @colors == "white"
+    @color == "white"
   end
 
   def say(x)
     "**;* #{x} **;*"
+  end
 
   def fly
     if @eating
@@ -28,5 +29,4 @@ class Uncorn
     @eating = true
     "**;* yummmm cotton candy **;*"
   end
-
 end
